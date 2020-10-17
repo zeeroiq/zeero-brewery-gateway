4adb8da2c73ab0d6eee0c4c9fc9d5e1651fba685
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-18 02:30:19 +0530

    project initialization files. closes #1

---
+++ src/test/java/com/shri/gateway/ZeeroBreweryGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shri.gateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ZeeroBreweryGatewayApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
