04c0014d72ced76da5e45f196eea441ca8931bab
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-21 02:17:55 +0530

    route config for inventory-service. closes #6

--- src/main/java/com/shri/gateway/config/LocalHostRoutingConfig.java
+++ src/main/java/com/shri/gateway/config/LocalHostRoutingConfig.java
@@ -21,6 +21,9 @@ public class LocalHostRoutingConfig {
                 .route(r -> r.path("/api/v1/customers/**", "/api/v1/customers*")
                         .uri("http://localhost:8082")
                         .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("http://localhost:8081")
+                        .id("order-service"))
                 .build();
     }
 }
