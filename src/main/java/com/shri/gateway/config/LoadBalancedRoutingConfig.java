9ba3f6a057eb9f61f1113c476b436692e7a82197
zeero1q <shrikantvishwakrma74@gmail.com>
2020-10-26 06:53:04 +0530

    configuration to use eureka. closes #7

---
+++ src/main/java/com/shri/gateway/config/LoadBalancedRoutingConfig.java
@@ -0,0 +1,31 @@
+/*
+ * Created by zeeroiq on 10/18/20, 5:12 PM
+ */
+
+package com.shri.gateway.config;
+
+import org.springframework.cloud.gateway.route.RouteLocator;
+import org.springframework.cloud.gateway.route.builder.RouteLocatorBuilder;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+@Profile("local-discovery")
+@Configuration
+public class LoadBalancedRoutingConfig {
+
+    @Bean
+    public RouteLocator loadBalancedRoutes(RouteLocatorBuilder builder) {
+        return builder.routes()
+                .route(r -> r.path("/api/v1/beer*", "/api/v1/beer/*", "/api/v1/beerUpc/*")
+                        .uri("lb://beer-service")
+                        .id("beer-service"))
+                .route(r -> r.path("/api/v1/customers/**", "/api/v1/customers*")
+                        .uri("lb://order-service")
+                        .id("order-service"))
+                .route(r -> r.path("/api/v1/beer/*/inventory")
+                        .uri("lb://inventory-service")
+                        .id("inventory-service"))
+                .build();
+    }
+}
